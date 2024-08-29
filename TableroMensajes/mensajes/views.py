6c1ee5c364fe7824d798193f42be09a7327c7e94
gabitoeo <gebelen@udc.edu.ar>
2024-08-29 19:57:58 -0300

    change

--- TableroMensajes/mensajes/views.py
+++ TableroMensajes/mensajes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Mensaje
 
 def mensajes_recibidos(request):
-    mensajes = Mensaje.objects.filter(destinatario='usuario')  # Ajusta según el contexto
+    mensajes = Mensaje.objects.filter(destinatario='usuario')
     return render(request, 'mensajes/mensajes_recibidos.html', {'mensajes': mensajes})
