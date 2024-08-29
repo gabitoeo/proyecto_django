6ec0642b37dca62195925d52b9d7453d43f4c207
gabitoeo <gebelen@udc.edu.ar>
2024-08-29 19:35:06 -0300

    vistas, urls y html

--- TableroMensajes/mensajes/models.py
+++ TableroMensajes/mensajes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Mensaje(models.Model):
+    texto = models.TextField()
+    remitente = models.ForeignKey(User, related_name='mensajes_enviados', on_delete=models.CASCADE)
+    destinatario = models.ForeignKey(User, related_name='mensajes_recibidos', on_delete=models.CASCADE)
+    fecha_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"De: {self.remitente} - Para: {self.destinatario} - {self.fecha_envio}"
--- TableroMensajes/mensajes/views.py
+++ TableroMensajes/mensajes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Mensaje
+from django.contrib.auth.models import User
 
-# Create your views here.
+def mensajes_recibidos(request):
+        request.user.is_authenticated:
+        mensajes = Mensaje.objects.filter(destinatario=request.user)
+        return render(request, 'mensajes/mensajes_recibidos.html', {'mensajes': mensajes})
