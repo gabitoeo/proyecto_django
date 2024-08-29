c1cbe94cbce687ef72d1a7592cf8473d7e0399b5
gabitoeo <gebelen@udc.edu.ar>
2024-08-29 19:36:00 -0300

    view problem solved

--- TableroMensajes/mensajes/views.py
+++ TableroMensajes/mensajes/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Mensaje
 from django.contrib.auth.models import User
 
 def mensajes_recibidos(request):
-        request.user.is_authenticated:
+    if request.user.is_authenticated:
         mensajes = Mensaje.objects.filter(destinatario=request.user)
         return render(request, 'mensajes/mensajes_recibidos.html', {'mensajes': mensajes})
+    else:
+        return redirect('login')
+    
